d6d3557126730d7f2665f7bf1603ac92507b23e0
ANZAD <anzadibayana@gmail.com>
2025-05-22 14:47:59 +0300

    Update main-ping.js

--- lazackcmds/main-ping.js
+++ lazackcmds/main-ping.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
     message: {
       contactMessage: {
-        displayName: `KELLY MD`,
+        displayName: `ANZADY MD`,
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'DENZEL MD'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
       },
     },
@@ -26,7 +26,7 @@ let handler = async (m, { conn }) => {
           key: pingMsg.key,
           type: 14,
           editedMessage: {
-            conversation: `*KELLY MD  ☠️⚡ running ping:* *${latency} ms*`,
+            conversation: `*ANZADY MD  ☠️⚡ running ping:* *${latency} ms*`,
           },
         },
       },
