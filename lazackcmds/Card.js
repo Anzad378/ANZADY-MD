a19483eefc3e4eb19ae488c3ee4e9db041561345
ANZAD <anzadibayana@gmail.com>
2025-05-23 07:40:57 +0300

    Update Card.js

--- lazackcmds/Card.js
+++ lazackcmds/Card.js
@@ -1,14 +1,14 @@
 let handler = async m =>
   m.reply(
     `⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣷⣜⢿⣧⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠻⣿⣿⣿⣿⣦⠄⠄
-乂───『 *LOFT XMD S24*』───乂 
-> Library : LOFT XMD
+乂───『 *ANZADY MD *』───乂 
+> Library : ANZADY MD
 > Version : 1.0.0
-> source : https://github.com/LOFTXMD23/loft-xmd-s24
+> source : https://github.com/Anzad378/ANZADY-MD
 
       if you find error or want to upgrade to premium plan contact to the owner.
       
-> LOFTXMD is bot made up by SIR LOFT 
+> ANZADY MD is bot made up by ANZADY MD
   __________________________
 
 
